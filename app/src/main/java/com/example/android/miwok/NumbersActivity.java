58010fed9e9935a912c569264cb0748d284f86b7
Jballi17 <palti2014@gmail.com>
2018-02-14 12:15:38 -0600

    Added String array and for loop to populate

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -8,5 +8,14 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //  Add code here for Numbers String array
+        String [] words = new String[10];
+
+        //  For loop to populate array with english numbers
+        for(int i=0;i < words.length;i++){
+            words[i]="@string/number_" + (i+1);
+        }
+        
     }
 }
