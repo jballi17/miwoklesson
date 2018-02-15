1c630a41cdb43fa5eeb54cdd6d7255724df876ce
Jballi17 <palti2014@gmail.com>
2018-02-14 18:06:18 -0600

    Modified to use ArrayAdapter for optimization

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +33,12 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(getString(R.string.numbers_ten));
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-        TextView wordView = new TextView(this);
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
