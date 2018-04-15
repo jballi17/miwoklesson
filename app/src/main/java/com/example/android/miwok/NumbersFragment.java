cb9043eae55f4bd3b0f35aeac65ae83721ed8ad7
Jballi17 <palti2014@gmail.com>
2018-04-14 19:04:02 -0500

    Moved NumbersActivity functionality into NumbersFragment

--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -1,18 +1,57 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NumbersFragment extends Fragment {
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
 
+    private MediaPlayer.OnCompletionListener mCompletionListener =
+            new MediaPlayer.OnCompletionListener(){
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    releaseMediaPlayer();
+                }
+            };
+
+    //  Create the AudioFocusChangeListener and it's state responses
+    //  to handle media and it's focus changes to play/pause,
+    //  and handle timing of releasing media resources
+    private AudioManager.OnAudioFocusChangeListener mAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Pause playback immediately
+                        mMediaPlayer.pause();
+                        //  Reset playback to it's beginning
+                        mMediaPlayer.seekTo(0);
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Begin playback
+                        mMediaPlayer.start();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        //  AudioFocus is lost, release mMediaPlayer and resources
+                        releaseMediaPlayer();
+                    }
+                }
+            };
 
     public NumbersFragment() {
         // Required empty public constructor
@@ -22,9 +61,95 @@ public class NumbersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        //  Create AudioManager
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        //  Add code here for Numbers ArrayList<Word>
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        //  Populate ArrayList<Word> with english numbers
+        words.add(new Word("one", "lutti", R.drawable.number_one,
+                R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two,
+                R.raw.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three,
+                R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four,
+                R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five,
+                R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six,
+                R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven,
+                R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight,
+                R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine,
+                R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha", R.drawable.number_ten,
+                R.raw.number_ten));
+
+        WordAdapter adapter = new WordAdapter(getActivity(),words,R.color.category_numbers);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        // Set onClickListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                releaseMediaPlayer();
+                mMediaPlayer = MediaPlayer.create(getActivity(),
+                        words.get(i).getAudioResourceID());
+
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback
+                    mMediaPlayer.start();
+                    //  Set onCompletionListener to release memory after sound finishes playing
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            //  Release Audio Focus when finished with music playing
+            mAudioManager.abandonAudioFocus(mAudioFocusChangeListener);
+        }
+    }
 }
